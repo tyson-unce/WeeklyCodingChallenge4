1acef981964462a668b485c07e7325b98f3ae3da
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-04-25 13:11:30 -0500

    updated easy to a multi-D array

--- WeeklyCodingChallenge_4/Easy/Easy.js
+++ WeeklyCodingChallenge_4/Easy/Easy.js
@@ -1,3 +1,3 @@
-const listOfStudents = ["Unce", "Tyson", "20", "Kong", "King", "400", "Long", "Jake", "15"]
+const listOfStudents = [["Unce", "Tyson", "20"],["Kong", "King", "400"], ["Long", "Jake", "15"]]
 
-console.log(`Hello, my name is ${listOfStudents[4]} ${listOfStudents[3]} and I'm ${listOfStudents[5]} years old.`)
+console.log(`Hello, my name is ${listOfStudents[1][1]} ${listOfStudents[1][0]} and I'm ${listOfStudents[1][2]} years old.`)
